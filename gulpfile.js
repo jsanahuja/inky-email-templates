2b4e92757273765fc95352a5c219343921e8d09d
jsanahuja <bannss1@gmail.com>
2020-06-05 20:56:35 +0200

    Removed unused dev dependency

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const inlineCss = require('gulp-inline-css');
 const gap = require('gulp-append-prepend');
 const htmlmin = require('gulp-html-minifier-terser');
 const livereload = require('gulp-livereload');
-const rename = require('gulp-rename');
 const inky = require('inky');
 
 const before_template = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
