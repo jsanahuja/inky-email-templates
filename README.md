7c1346737e791f3877c8f0f17a503d26d9bc5854
jsanahuja <bannss1@gmail.com>
2020-06-05 20:49:21 +0200

    liverelaod, index generation & docs

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# email-templates
-Email templates development environment
+# inky-email-templates
+[Inky](https://github.com/foundation/inky) & [foundation-emails](https://github.com/foundation/foundation-emails) development environment.
+
+## Setup
+> git clone https://github.com/jsanahuja/inky-email-templates.git
+
+> npm install
+
+## Examples
+
+See some examples [clicking here](https://www.sowecms.com/demos/inky-email-templates/)
+
+## Usage
+
+### For development
+
+- **dev**: Watches for any change in `src/*.html` to automatically perform a fast compilation into `build/$1.html` and reload the browser.
+> npm run dev
+
+- **examples**: Copies all the [foundation-emails templates](https://github.com/foundation/foundation-emails/tree/develop/templates) into `foundation-examples/src` and builds them into `foundation-examples/build` so you can see both the source and result of each.
+> npm run examples
+
+- **index**: Generates the `index.html` file with a list of your builds. Check this [index.html example](https://www.sowecms.com/demos/inky-email-templates/)
+> npm run index
+
+### For production
+- **build**: Compiles all the html files in `src/` into `build`. This method also transforms all the CSS files into inline CC and minifies both HTML and CSS.
+> npm run build
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const inlineCss = require('gulp-inline-css');
 const gap = require('gulp-append-prepend');
 const htmlmin = require('gulp-html-minifier-terser');
+const livereload = require('gulp-livereload');
+const rename = require('gulp-rename');
 const inky = require('inky');
 
 const before_template = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -35,23 +37,74 @@ gulp.task('build-examples', gulp.series('copy-examples', function () {
         .pipe(gulp.dest('./foundation-examples/build'));
 }));
 
-gulp.task('dev', function () {
+gulp.task('build', function () {
     return gulp.src('src/*.html')
         .pipe(gap.prependText(before_template))
         .pipe(gap.appendText(after_template))
         .pipe(inky())
+        .pipe(inlineCss())
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            minifyCSS: true
+        }))
         .pipe(gulp.dest('build/'));
 });
 
-// gulp.task('build', function () {
-//     return gulp.src('src/*.html')
-//         .pipe(gap.prependText(before_template))
-//         .pipe(gap.appendText(after_template))
-//         .pipe(inky())
-//         .pipe(inlineCss())
-//         .pipe(htmlmin({
-//             collapseWhitespace: true,
-//             minifyCSS: true
-//         }))
-//         .pipe(gulp.dest('build/'));
-// });
+gulp.task('dev-build', function () {
+    console.log("Building...");
+    return gulp.src('src/*.html')
+        .pipe(gap.prependText(before_template))
+        .pipe(gap.appendText(`<script src="../node_modules/livereload-js/dist/livereload.js?host=localhost"></script>`))
+        .pipe(gap.appendText(after_template))
+        .pipe(inky())
+        .pipe(gulp.dest('build/'))
+        .pipe(livereload());
+});
+
+gulp.task('dev', gulp.series('dev-build', function(){
+    livereload.listen({ start: true });
+    gulp.watch("src/*.html", gulp.series('dev-build'));
+}));
+
+function index(callback){
+    const buildPath = path.join(__dirname, 'build');
+    const examplesPath = path.join(__dirname, 'foundation-examples/build');
+
+    var template = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+    <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <title>inky-email-templates</title>
+        </head>
+        <body>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="my-5"><a href="https://github.com/jsanahuja/inky-email-templates" target="_blank">inky-email-templates</a></h1>
+    `;
+
+    fs.readdir(buildPath, function (err, files) {
+        if(files && files.length){
+            template += "<h2>Builds</h2><ul>";
+            files.forEach(function (file) {
+                // Do whatever you want to do with the file
+                template += '<li><a href="build/' + file + '" target="_blank">' + file + '</a></li>';
+            });
+            template += "</ul>";
+        }
+        fs.readdir(examplesPath, function (err, files) {
+            if(files && files.length){
+                template += "<h2>Foundation-emails Examples</h2><ul>";
+                files.forEach(function (file) {
+                    // Do whatever you want to do with the file
+                    template += '<li><a href="foundation-examples/build/' + file + '" target="_blank">' + file + '</a></li>';
+                });
+                template += "</ul>";
+            }
+            fs.writeFileSync("./index.html", template);
+            callback();
+        });
+    });
+};
+
+exports.index = index;
